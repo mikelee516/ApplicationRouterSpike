a0d1e87f85d85c38ffb62b0de4d39e3a35ef7955
mikelee516 <mike@mikelee.net>
2014-02-02 23:46:43 -0500

    Initial check in of application code
    Initial version of the AngularJS client
    Initial version of the Application Router

---
+++ ApplicationRouter/ApplicationRouter/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Configuration;
+using System.Net.Http;
+
+namespace ApplicationRouter
+{
+    class Program
+    {
+        static void Main()
+        {
+            var port = int.Parse(ConfigurationManager.AppSettings["port"]);
+            var baseAddress = string.Format("http://localhost:{0}/", port);
+
+            // Start OWIN host 
+            using (WebApp.Start<Startup>(url: baseAddress))
+            {
+                // Create HttpCient and make a request to api/values 
+                var client = new HttpClient();
+
+                var response = client.GetAsync(baseAddress + "api/values").Result;
+
+                Console.WriteLine(response);
+                Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+
+                Console.ReadLine();
+            }
+        } 
+    }
+}
---
+++ FoodTrackerUI/scripts/foodTracker.js
@@ -0,0 +1,12 @@
+var app = angular.module('foodTracker', []);
+
+app.factory('endpointUrlResolver', function(){
+  var applicationRouterUrl = '';
+  return function(){
+    
+  };
+});
+
+function FoodTrackerCtrl($scope) {
+
+};
