61435309287a95047643bb4c1fb5aea796d9f7c8
mjlee <michael.lee@guggenheimpartners.com>
2014-02-04 14:50:00 -0500

    Updated SQL scripts for tables

---
+++ database/tables/FoodTracker.Meals.sql
@@ -0,0 +1,24 @@
+USE [FoodTracker]
+GO
+
+/****** Object:  Table [dbo].[Meals]    Script Date: 02/04/2014 14:47:49 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Meals](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[UserId] [int] NOT NULL,
+	[FoodId] [int] NOT NULL,
+	[Time] [datetime] NOT NULL,
+ CONSTRAINT [PK_Meals] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+
